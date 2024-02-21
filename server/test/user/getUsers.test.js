14fef66e7056e7606743b7fe3930573f07d388a0
Arkadiusz Suchenia <suchenia.arkadiusz@aru-software.pl>
2024-02-21 19:34:05 +0100

    Revert changes to pass the build

--- server/test/user/getUsers.test.js
+++ server/test/user/getUsers.test.js
@@ -26,7 +26,7 @@ describe('API getUsers', () => {
     const response = await request(app)
       .get('/api/v1/users')
       .set({ Authorization: `Bearer ${validToken}` })
-      .expect(300);
+      .expect(200);
 
     expect(response.body.length).toBe(1);
   });
