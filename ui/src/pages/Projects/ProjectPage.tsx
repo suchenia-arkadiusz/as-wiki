11d85a9679c974aa6ba39f815f2e1e9426c6ffe5
Arkadiusz Suchenia <suchenia.arkadiusz@aru-software.pl>
2024-03-13 21:03:11 +0100

    feat[#83] - Edit project from the project's page view

--- ui/src/pages/Projects/ProjectPage.tsx
+++ ui/src/pages/Projects/ProjectPage.tsx
@@ -5,20 +5,39 @@ import { type Project } from './types.ts';
 import Loader from '../../components/Loader/Loader.tsx';
 import { useRestApiContext } from '../../contexts/RestApiContext.tsx';
 import MDPreview from '../../components/MDEditor/MDPreview.tsx';
+import Button from '../../components/Button/Button.tsx';
+import CreateProjectPopup from './components/CreateProjectPopup/CreateProjectPopup.tsx';
 
 const ProjectPageContainer = styled.div`
   display: flex;
   flex-direction: column;
   padding: 30px;
+  width: 100%;
+`;
+
+const ProjectPageEditButtonContainer = styled.div`
+  position: absolute;
+  background: none;
+  right: 30px;
 `;
 
 const ProjectPage = () => {
   const params = useParams();
   const [isLoaded, setIsLoaded] = useState<boolean>(false);
   const [project, setProject] = useState<Project>();
+  const [isEditPopupOpen, setIsEditPopupOpen] = useState<boolean>(false);
   const api = useRestApiContext();
 
   useEffect(() => {
+    getProject();
+  }, []);
+
+  const closePopup = () => {
+    setIsEditPopupOpen(false);
+    getProject();
+  };
+
+  const getProject = () => {
     setIsLoaded(false);
     api.get(`/api/v1/projects/${params.id}`).then((response) => {
       if (response.status === 200) {
@@ -28,7 +47,7 @@ const ProjectPage = () => {
         });
       }
     });
-  }, []);
+  };
 
   return (
     <>
@@ -36,11 +55,19 @@ const ProjectPage = () => {
         ? (
           <ProjectPageContainer data-testid='ProjectPage.container'>
             <h1>{project?.name.toUpperCase()}</h1>
+            <ProjectPageEditButtonContainer>
+              <Button
+                data-testid="ProjectPage.button.editProject"
+                iconName="bi-pen"
+                onClick={() => {setIsEditPopupOpen(true);}}
+              />
+            </ProjectPageEditButtonContainer>
             <p>
               <strong>Total number of pages: </strong>
               {project?.numberOfPages}
             </p>
             <MDPreview value={project?.description || ''} />
+            {isEditPopupOpen ? <CreateProjectPopup onClose={closePopup} selectedProject={project} isEdit /> : null}
           </ProjectPageContainer>
         )
         : (
--- ui/test/pages/Projects/ProjectPage.test.tsx
+++ ui/test/pages/Projects/ProjectPage.test.tsx
@@ -1,6 +1,6 @@
 import ProjectPage from '../../../src/pages/Projects/ProjectPage.tsx';
 import MockBrowser from '../../mocks/MockBrowser.tsx';
-import { render, waitFor } from '@testing-library/react';
+import { act, render, waitFor } from '@testing-library/react';
 import { expect, vi } from 'vitest';
 
 const apiHandler = {
@@ -34,5 +34,42 @@ describe('<ProjectPage />', () => {
     expect(screen.getByText('TEST PROJECT')).toBeInTheDocument();
     expect(screen.getByText('Test Description')).toBeInTheDocument();
     expect(screen.getByText('10')).toBeInTheDocument();
+    expect(screen.getByTestId('ProjectPage.button.editProject')).toBeInTheDocument();
+  });
+
+  it('should open edit popup when edit button is clicked', async () => {
+    const screen = setupScreen();
+
+    await waitFor(() => {
+      expect(screen.getByTestId('ProjectPage.container')).toBeInTheDocument();
+    });
+
+    act(() =>
+      screen.getByTestId('ProjectPage.button.editProject').click()
+    );
+
+    expect(screen.getByTestId('CreateProject.container')).toBeInTheDocument();
+    expect(screen.getByText('TEST PROJECT')).toBeInTheDocument();
+    expect(screen.getByText('10')).toBeInTheDocument();
+  });
+
+  it('should close edit popup when close button is clicked', async () => {
+    const screen = setupScreen();
+
+    await waitFor(() => {
+      expect(screen.getByTestId('ProjectPage.container')).toBeInTheDocument();
+    });
+
+    act(() =>
+      screen.getByTestId('ProjectPage.button.editProject').click()
+    );
+
+    expect(screen.getByTestId('CreateProject.container')).toBeInTheDocument();
+
+    act(() =>
+      screen.getByTestId('Popup.close.button').click()
+    );
+
+    expect(screen.queryByTestId('CreateProject.container')).not.toBeInTheDocument();
   });
 });
