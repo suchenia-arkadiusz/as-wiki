9cd761e96ffee4cfec8d13139832e703b77e14c9
Arkadiusz Suchenia <suchenia.arkadiusz@aru-software.pl>
2024-04-08 21:02:31 +0200

    Async run the add USERS table

--- server/migrations/20231010190457-V1-Baseline.js
+++ server/migrations/20231010190457-V1-Baseline.js
@@ -22,18 +22,18 @@ exports.setup = function (options, seedLink) {
 };
 
 exports.up = function (db) {
-  createUsersTableAndAdminUser(db);
+  createUsersTableAndAdminUser(db).then(() => {
+    const filePath = path.join(__dirname, 'sqls', '20231010190457-V1-Baseline-up.sql');
+    return new Promise(function (resolve, reject) {
+      fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
+        if (err) return reject(err);
+        console.log(`received data: ${  data}`);
 
-  const filePath = path.join(__dirname, 'sqls', '20231010190457-V1-Baseline-up.sql');
-  return new Promise(function (resolve, reject) {
-    fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
-      if (err) return reject(err);
-      console.log(`received data: ${  data}`);
-
-      resolve(data);
+        resolve(data);
+      });
+    }).then(function (data) {
+      return db.runSql(data);
     });
-  }).then(function (data) {
-    return db.runSql(data);
   });
 };
 
@@ -55,7 +55,7 @@ exports._meta = {
   version: 1,
 };
 
-function createUsersTableAndAdminUser(db) {
+async function createUsersTableAndAdminUser(db) {
   bcrypt.hash(process.env.APP_ADMIN_USER_PASSWORD, 12).then((hashedPassword) => {
     const createUsersTableQuery = `CREATE TABLE "USERS" (
     "id" uuid PRIMARY KEY ,
